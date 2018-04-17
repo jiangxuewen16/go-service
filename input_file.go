f013a053fafef1455b0b0de17bd3d5ec296a13ab
jiangxuewen <445251692@qq.com>
2018-04-17 09:58:20 +0800

    第一次提交

--- input_file.go
+++ input_file.go
@@ -7,30 +7,32 @@ import (
 	"fmt"
 	"time"
 )
+
 var exceptionSlice = [...]string{"com.carhouse.common.exception.ServiceRuntimeException: 404找不到 — 服务器找不到给定的资源；文档不存在\n",
-"java.lang.NullPointerException: 空指针\n"}
+	"java.lang.NullPointerException: 空指针\n"}
 
 func main() {
-	for {
-		go writeStr()
-		time.Sleep(time.Second * 5)
-	}
-}
 
-func writeStr()  {
-	randNum := rand.Intn(1)
-	str := exceptionSlice[randNum]
 	fileName := "D:/logs/log.log"
-	f, err := os.OpenFile(fileName,os.O_APPEND, 0666)
-
-	defer f.Close()
+	f, err := os.OpenFile(fileName, os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	n, err1 := io.WriteString(f, str)
-	if err1 != nil {
-		fmt.Println(err)
+	defer f.Close()
+	for {
+		randNum := rand.Intn(2)
+		str := exceptionSlice[randNum]
+		writeStr(f,str)
+		time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Printf("写入 %d 个字节\n", n)
+}
+
+func writeStr(f io.Writer, str string) error {
+
+	_, err := io.WriteString(f, str)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("写入 【%d】\n", str)
+	return nil
 }
