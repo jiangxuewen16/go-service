6955f50d98f1855277276ad8be269ea5f10943fb
DESKTOP-094KS21\jiang <445251692@qq.com>
2018-04-17 22:53:31 +0800

    修改

--- input_file.go
+++ input_file.go
@@ -6,24 +6,45 @@ import (
 	"os"
 	"fmt"
 	"time"
+	"flag"
 )
 
+var fileName = flag.String("f", "/home/logs/golang/test.log", "文件路径+文件名称")
+var sleepTime = flag.Int("t", 10, "写入间隔时间，单位是毫秒")
+var stringType = flag.Int("T", 0, "写入类型，0-随机，1-写入第一个，2-写入第二个")
 var exceptionSlice = [...]string{"com.carhouse.common.exception.ServiceRuntimeException: 404找不到 — 服务器找不到给定的资源；文档不存在\n",
 	"java.lang.NullPointerException: 空指针\n"}
 
 func main() {
-
-	fileName := "D:/logs/log.log"
-	f, err := os.OpenFile(fileName, os.O_APPEND, 0666)
+	flag.Parse()
+	if len(*fileName) == 0 {
+		fmt.Println("文件名称不能为空")
+		os.Exit(0)
+	}
+	if str := exceptionSlice[*stringType]; len(str) <= 0 {
+		fmt.Println("请填写有效数字")
+		os.Exit(0)
+	}
+	f, err := os.OpenFile(*fileName, os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(0)
 	}
 	defer f.Close()
 	for {
-		randNum := rand.Intn(2)
+		if *sleepTime <= 0 {
+			fmt.Println("写入间隔时间必须大于0")
+			os.Exit(0)
+		}
+		time.Sleep(time.Millisecond * time.Duration(*sleepTime))
+
+		randNum := rand.Intn(len(exceptionSlice))
 		str := exceptionSlice[randNum]
-		writeStr(f,str)
-		time.Sleep(time.Millisecond * 100)
+		err := writeStr(f, str)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
 	}
 }
 
